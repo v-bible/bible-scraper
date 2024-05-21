704826f28789920e6675f2683b014507c026a3ed
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-05-21 12:33:23 +0700

    refactor(bible): setup main.ts

---
+++ apps/bible/src/main.ts
@@ -0,0 +1,64 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+import { getVerse } from './get-verse';
+import { getBook } from '@/get-book';
+import { getVersion } from '@/get-version';
+import prisma from '@/prisma/prisma';
+
+(async () => {
+  await getVersion();
+
+  const version = await prisma.version.findFirstOrThrow({
+    where: {
+      code: 'BD2011',
+    },
+    include: {
+      formats: true,
+    },
+  });
+
+  const versionFormat = await prisma.versionFormat.findFirstOrThrow({
+    where: {
+      versionId: version.id,
+      type: 'ebook',
+    },
+  });
+
+  await getBook({
+    type: versionFormat.type,
+    url: versionFormat.url,
+  });
+
+  const books = await prisma.book.findMany({
+    where: {
+      versionId: version.id,
+    },
+  });
+
+  for (const book of books) {
+    const chapters = await prisma.bookChapter.findMany({
+      where: {
+        bookId: book.id,
+      },
+      include: {
+        book: true,
+      },
+    });
+
+    for (const chap of chapters) {
+      // const verseChap = await prisma.bookVerse.findFirst({
+      //   where: {
+      //     chapterId: chap.id,
+      //   },
+      // });
+
+      // if (!verseChap) {
+      //   logger.info(`skipping ${chap.book.title} ${chap.number}`);
+      //   continue;
+      // }
+
+      await getVerse(chap);
+    }
+  }
+})();
