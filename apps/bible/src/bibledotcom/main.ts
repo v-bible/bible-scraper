4712101450ff2a596a9543184d2060ea4cbd56ce
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2024-05-25 21:34:09 +0700

    refactor(bible): move main.ts

---
+++ apps/bible/src/bibledotcom/main.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+import { getBook } from '@/bibledotcom/get-book';
+import { getFootnote } from '@/bibledotcom/get-footnote';
+import { getHeading } from '@/bibledotcom/get-heading';
+import { getVerse } from '@/bibledotcom/get-verse';
+import { getVersionByLang } from '@/bibledotcom/get-version';
+import prisma from '@/prisma/prisma';
+
+(async () => {
+  // NOTE: You can use "getVersion" func to scrap all the versions available.
+  await getVersionByLang('vie');
+
+  const version = await prisma.version.findFirstOrThrow({
+    where: {
+      code: 'BD2011',
+    },
+    include: {
+      formats: true,
+    },
+  });
+
+  const versionFormat = await prisma.versionFormat.findFirstOrThrow({
+    where: {
+      versionId: version.id,
+      type: 'ebook',
+    },
+    include: {
+      version: true,
+    },
+  });
+
+  await getBook(versionFormat);
+
+  const books = await prisma.book.findMany({
+    where: {
+      versionId: version.id,
+    },
+  });
+
+  for (const book of books) {
+    const chapters = await prisma.bookChapter.findMany({
+      where: {
+        bookId: book.id,
+      },
+      include: {
+        book: true,
+      },
+    });
+
+    for (const chap of chapters) {
+      // const verseChap = await prisma.bookVerse.findFirst({
+      //   where: {
+      //     chapterId: chap.id,
+      //   },
+      // });
+
+      // if (!verseChap) {
+      //   logger.info(`skipping ${chap.book.title} ${chap.number}`);
+      //   continue;
+      // }
+
+      await getVerse(chap);
+      await getFootnote(chap);
+      await getHeading(chap);
+    }
+  }
+})();
