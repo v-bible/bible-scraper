7adb6dc26d3ca503e90f2fe1c39ad8c2100faf0f
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-04-14 08:52:09 +0700

    fix(prisma): update migrations

--- prisma/pg/migrations/20250227101314_add_book_heading_level/migration.sql
+++ prisma/pg/migrations/20250227101314_add_book_heading_level/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE "book_heading" ADD COLUMN     "level" INTEGER NOT NULL DEFAULT 1;
+ALTER TABLE "book_heading" ADD COLUMN     "level" INTEGER NOT NULL DEFAULT 0;
--- prisma/sqlite/migrations/20250227100827_add_book_heading_level/migration.sql
+++ prisma/sqlite/migrations/20250227100827_add_book_heading_level/migration.sql
@@ -4,7 +4,7 @@ PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_book_heading" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "content" TEXT NOT NULL,
-    "level" INTEGER NOT NULL DEFAULT 1,
+    "level" INTEGER NOT NULL DEFAULT 0,
     "order" INTEGER NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -13,7 +13,7 @@ CREATE TABLE "new_book_heading" (
     CONSTRAINT "book_heading_chapterId_fkey" FOREIGN KEY ("chapterId") REFERENCES "book_chapter" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "book_heading_verseId_fkey" FOREIGN KEY ("verseId") REFERENCES "book_verse" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
-INSERT INTO "new_book_heading" ("chapterId", "content", "createdAt", "id", "order", "updatedAt", "verseId") SELECT "chapterId", "content", "createdAt", "id", "order", "updatedAt", "verseId" FROM "book_heading";
+INSERT INTO "new_book_heading" ("chapterId", "content", "createdAt", "id", "level", "order", "updatedAt", "verseId") SELECT "chapterId", "content", "createdAt", "id", 0 AS "level", "order", "updatedAt", "verseId" FROM "book_heading";
 DROP TABLE "book_heading";
 ALTER TABLE "new_book_heading" RENAME TO "book_heading";
 CREATE UNIQUE INDEX "book_heading_order_verseId_key" ON "book_heading"("order", "verseId");
